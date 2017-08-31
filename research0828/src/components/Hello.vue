c5bf332b0d7ac5087c6050c1b196ea90659725f2
xinlong0918 <fan.xinlong@jyall.com>
2017-08-31 11:39:08 +0800

    考勤--八月底

---
+++ research0828/src/components/Hello.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="hello">
+        <router-link to="/picker">picker</router-link>
+
+        <transition name="slide-fade" class="fadeView">
+            <div v-if="show">
+                <image-view :imgArr="imgArr"
+                            :showImageView="true"
+                            :imageIndex="imageIndex"
+                            v-on:hideImage="hideImageView"></image-view>
+            </div>
+        </transition>
+        <h1 @click="showImgView">显示隐藏</h1>
+        <img v-for="(item, index) in imgArr" :src="item" @click="selectImg(index)">
+    </div>
+</template>
+
+<script>
+    import imageView from 'vue-imageview'
+    export default {
+        name: 'hello',
+        components: {
+            'image-view': imageView
+        },
+        data () {
+            return {
+                // 图片数组
+                imgArr: [
+                    'https://image1.jyall.com/v1/tfs/T1yyATB7J_1R4n9VrK.gif',
+                    'http://static.jyall.com/jyall_shop/dest/home/images/app100x100.jpg',
+                    'http://img.blog.csdn.net/20160526104323632'
+                ],
+                // 显示组件
+                show: false,
+                // 从哪一张图片开始
+                imageIndex: 0
+            }
+        },
+        methods: {
+            showImgView () {
+                this.show = true
+            },
+            hideImageView () {
+                this.show = false
+            },
+            selectImg (index) {
+                this.show = true
+                this.imageIndex = index
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .slide-fade-enter-active {
+        transition: opacity .5s ease;
+    }
+
+    .slide-fade-leave-active {
+        transition: opacity .5s ease;
+    }
+
+    .slide-fade-enter, .slide-fade-leave-active {
+        opacity: 0;
+    }
+    h1, h2 {
+        margin: 0;
+        padding: 0;
+    }
+    img {
+        display: block;
+        margin: 10px auto;
+        max-width: 400px;
+    }
+</style>
---
+++ research0828/src/components/Picker.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <p @click = "setDate" >点击设置日期（默认今天）</p>
+        <p @click = "setDate2" >设定指定的日期（20150220）</p>
+        <p>{{data}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: '',
+        data () {
+            return {
+                data:'日期'
+            }
+        },
+
+        methods:{
+            setDate(){
+                let vm = this;
+                this.$calendar.show({
+                    startYear: 2016,
+                    endYear: 2115,
+                    onOk(data){
+                        console.log(data)
+                        vm.data= data.year+'-'+data.month+'-'+data.day;
+                        console.log('确定')
+                    },
+                    onCancel(){
+                        console.log('取消')
+                    }
+                })
+            },
+            setDate2(){
+                let vm = this;
+                this.$calendar.show({
+                    onOk(data){
+                        vm.data= data.year+'-'+data.month+'-'+data.day;
+                        console.log('确定')
+                    },
+                    onCancel(){
+                        console.log('取消')
+                    },
+                    year:2015,
+                    month:2,
+                    day:20,
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
