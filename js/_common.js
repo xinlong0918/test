a6983f4c72c9212abae8888804ee8b34ff2ab41e
xinlong0918 <fan.xinlong@jyall.com>
2018-01-15 10:26:04 +0800

    _equals

--- js/_common.js
+++ js/_common.js
@@ -15,13 +15,15 @@ function _equals (a, b) {
             var isArrayB = Array.isArray(b);
             if (isArrayA && isArrayB) {
                 return a.length === b.length && a.every(function (e, i) {
-                        return looseEqual(e, b[i])
+                        return _equals(e, b[i])
                     })
             } else if (!isArrayA && !isArrayB) {
                 var keysA = Object.keys(a);
                 var keysB = Object.keys(b);
                 return keysA.length === keysB.length && keysA.every(function (key) {
-                        return looseEqual(a[key], b[key])
+                        console.log(key)
+
+                        return _equals(a[key], b[key])
                     })
             } else {
                 /* istanbul ignore next */
@@ -37,3 +39,8 @@ function _equals (a, b) {
         return false
     }
 }
+function isObject(o) {
+    return o !== null && typeof o === 'object'
+}
+let result = _equals({a: '1'}, {a: 1})
+console.log(result)
